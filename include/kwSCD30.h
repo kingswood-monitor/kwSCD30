74ab39ea26463863251114d92175adbf7bfbeef6
Richard <richlyon@mac.com>
2021-11-02 11:58:59 +0000

    Replace temperature offset code
    Subtracts a defined offset from the temperature reading rather than calling the SCD30 temperatureOffset method.

--- include/kwSCD30.h
+++ include/kwSCD30.h
@@ -4,33 +4,28 @@
 #include "SparkFun_SCD30_Arduino_Library.h"
 
 typedef struct {
-  float temperature;
-  uint8_t humidity;
+  float    temperature;
+  uint8_t  humidity;
   uint16_t co2;
-} __attribute__((__packed__))payload_t;
+} __attribute__( ( __packed__ ) ) payload_t;
 
-class kwSCD30
-{
-public:
+class kwSCD30 {
+ public:
+  kwSCD30();
+  float    temperature();
+  uint16_t humidity();
+  uint16_t co2();
+  void     start();
+  bool     start( float temperatureOffset );
+  bool     dataAvailable();
+  bool     hasSCD30();
+  float    temperatureOffset();
 
-    kwSCD30();
-
-    bool start( float temperatureOffset );
-    bool dataAvailable();
-    bool hasSCD30();
-    float temperatureOffset();
-
-    float temperature();
-    uint16_t humidity();
-    uint16_t co2();
-
-    bool m_hasSCD30 = false;
-  
-protected:
-
-    SCD30 m_scd30;
-    payload_t m_payload;
-    float m_temperatureOffset;
+ protected:
+  SCD30     m_scd30;
+  payload_t m_payload;
+  bool      m_hasSCD30 = false;
+  float     m_temperatureOffset = 0;
 };
 
 #endif
--- src/kwSCD30.cpp
+++ src/kwSCD30.cpp
@@ -1,53 +1,46 @@
 #include "kwSCD30.h"
 
 // kwSCD30 constructor
-kwSCD30::kwSCD30()
-{
-}
+kwSCD30::kwSCD30() {}
+
+void kwSCD30::start() { start( 0 ); }
 
 // Start the sensor with maximum update frequency (2 seconds)
 bool kwSCD30::start( float temperatureOffset )
 {
-    m_temperatureOffset = temperatureOffset;
-    m_hasSCD30 = m_scd30.begin();
+  m_temperatureOffset = temperatureOffset;
+  m_hasSCD30 = m_scd30.begin();
 
-    if ( m_hasSCD30 )
-    {
-        m_scd30.setAutoSelfCalibration( true );
-        m_scd30.setMeasurementInterval(2);
-        m_scd30.setTemperatureOffset( temperatureOffset );
-    }
-    return m_hasSCD30;
+  if ( m_hasSCD30 )
+  {
+    m_scd30.setAutoSelfCalibration( true );
+    m_scd30.setMeasurementInterval( 2 );
+    m_scd30.setTemperatureOffset( 0 );
+  }
+  return m_hasSCD30;
 }
 
-bool kwSCD30::hasSCD30()
-{
-    return m_hasSCD30;
-}
+bool kwSCD30::hasSCD30() { return m_hasSCD30; }
 
-float kwSCD30::temperatureOffset()
-{
-    return m_temperatureOffset;
-}
+float kwSCD30::temperatureOffset() { return m_temperatureOffset; }
 
 // Update the readings if available
 bool kwSCD30::dataAvailable()
 {
-    if (m_hasSCD30 && m_scd30.dataAvailable())
-    {
-        m_payload.temperature = m_scd30.getTemperature();
-        m_payload.humidity = ( int )m_scd30.getHumidity();
-        m_payload.co2 = ( int )m_scd30.getCO2();
+  if ( m_hasSCD30 && m_scd30.dataAvailable() )
+  {
+    m_payload.temperature = m_scd30.getTemperature() - m_temperatureOffset;
+    m_payload.humidity = (int)m_scd30.getHumidity();
+    m_payload.co2 = (int)m_scd30.getCO2();
 
-        return true;
-    }
-    else
-    {
-        return false;
-    }
+    return true;
+  } else
+  {
+    return false;
+  }
 }
 
 // Readings
-float kwSCD30::temperature() { return m_payload.temperature; }
+float    kwSCD30::temperature() { return m_payload.temperature; }
 uint16_t kwSCD30::humidity() { return m_payload.humidity; }
 uint16_t kwSCD30::co2() { return m_payload.co2; }
